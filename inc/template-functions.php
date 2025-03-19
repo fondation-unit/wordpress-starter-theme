8891d28d4f9b285a79fae788b427da1a7488d4b5
ledob44 <olivier.imbert@unit.eu>
2025-03-19 14:30:01 +0100

    added embed no cookie youtube video function

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -57,3 +57,16 @@ add_filter('rest_endpoints', function ($endpoints) {
 
     return $endpoints;
 });
+
+function filter_youtube_embed($cached_html, $url = null)
+{
+
+    // Search for youtu to return true for both youtube.com and youtu.be URLs
+    if (strpos($url, 'youtu')) {
+        $cached_html = preg_replace('/youtube\.com\/(v|embed)\//s', 'youtube-nocookie.com/$1/', $cached_html);
+    }
+
+    return $cached_html;
+}
+
+add_filter('embed_oembed_html', 'filter_youtube_embed', 10, 2);
